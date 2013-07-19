c83abeba04391e9420f911b0ce06c66123eef32f
Meredith Underell <m.underell@gmail.com>
2013-07-18 18:33:29 -0700

    Add the ability to create users through the admin backend

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Ticketee::Application.routes.draw do
     resources :tickets
   end
 
+  namespace :admin do
+    root :to => "base#index"
+    resources :users
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

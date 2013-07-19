6853b93b4d74d19a22bfc1bd31be00dbbd898721
Meredith Underell <m.underell@gmail.com>
2013-07-18 18:06:49 -0700

    Add admin namespaced users controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -58,13 +58,7 @@ class ProjectsController < ApplicationController
 			redirect_to projects_path
 		end
 
-		def authorize_admin!
-			authenticate_user!
-			unless current_user.admin?
-				flash[:alert] = "You must be an admin to do that."
-				redirect_to root_path
-			end
-		end
+
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Ticketee::Application.routes.draw do
 
+  get "/admin/users/index"
+
   devise_for :users
 
   root :to => "projects#index"

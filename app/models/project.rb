35c72fdcd3a330bb835df8aae70785ceb9c07208
Meredith Underell <m.underell@gmail.com>
2013-07-20 21:06:03 -0700

    Make projects only visible to users with permission to see them

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,13 @@ class Project < ActiveRecord::Base
   has_many :tickets, :dependent => :delete_all
   has_many :tickets, :dependent => :nullify
 
-  validates :name, :presence => true
+  scope :admins, where(:admin => true)
+
+  validates :name, :presence => true, :uniqueness => true
+
+  has_many :permissions, :as => :thing
+  def self.viewable_by(user)
+    joins(:permissions).where(:permissions => { :action => "view",
+                                                :user_id => user.id })
+  end
 end
--- spec/integration/viewing_tickets_spec.rb
+++ spec/integration/viewing_tickets_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 feature 'Viewing tickets' do
   before do
+    user = Factory(:confirmed_user)
     textmate_2 = Factory(:project, :name => "TextMate 2")
-    user = Factory(:user)
+    define_permission!(user, "view", textmate_2)
     ticket = Factory( :ticket,
                       :project => textmate_2,
                       :title => "Make it shiny!",
@@ -11,12 +12,14 @@ feature 'Viewing tickets' do
     ticket.update_attribute(:user, user)
 
     internet_explorer = Factory(:project, :name => "Internet Explorer")
+    define_permission!(user, "view", internet_explorer)
 
     Factory(:ticket,
             :project => internet_explorer,
             :title => "Standards compliance",
             :description => "Isn't a joke.")
 
+    sign_in_as!(user)
     visit '/'
   end
 

8a8fec85f512f534a82f35528b176ed4481f5c1b
Meredith Underell <m.underell@gmail.com>
2013-08-05 15:22:16 -0700

    Assets are not strictly served through FilesController

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,8 @@
 class Asset < ActiveRecord::Base
   attr_accessible :asset
 
-  has_attached_file :asset
+  has_attached_file :asset, :path => (Rails.root + "files/:id").to_s,
+                            :url => "/files/:id"
+
+  belongs_to :ticket
 end
